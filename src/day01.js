a2aa55e36be97bdda8301ec5e14848836a1e789e
John Harlow <info@jharlow.uk>
2022-12-01 19:39:46 -0800

    Day 1: 2 gold stars

---
+++ src/day01.js
@@ -0,0 +1,22 @@
+import fs from "fs";
+
+const day01 = () => {
+  const data = fs.readFileSync("./resources/day01.input.txt", "utf-8");
+  const emptyNewLineRegex = /(?:\h*\n){2,}/;
+
+  const caloriesSplitByElf = data.split(emptyNewLineRegex);
+
+  const totalCaloriesSplitByElf = caloriesSplitByElf
+    .map((setOfCalories) => {
+      const arrayOfCalories = setOfCalories
+        .split(/\n/)
+        .map((val) => Number(val));
+      const caloriesTotal = arrayOfCalories.reduce((a, b) => a + b);
+      return caloriesTotal;
+    })
+    .sort((a, b) => (a > b ? -1 : 1));
+
+  return totalCaloriesSplitByElf.splice(0, 3).reduce((a, b) => a + b, 0);
+};
+
+console.log(day01());
