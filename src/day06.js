68ebb4c89a9447e566217ba49145370b4414819f
John Harlow <info@jharlow.uk>
2022-12-06 08:59:41 -0800

    Day 6: 2x ⭐

--- src/day06.js
+++ src/day06.js
@@ -3,8 +3,6 @@ import fs from "fs";
 const data = fs.readFileSync("./resources/day06.input.txt", "utf-8").split("");
 
 const day06 = (distinct) => {
-  if (typeof distinct !== "number")
-    throw new Error("Must provide length of distinct marker!");
   return data.findIndex((_, i) => {
     if (i <= distinct - 1) return false;
     const marker = data.slice(i - distinct, i);
