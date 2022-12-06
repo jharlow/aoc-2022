ac18aa0e48afa05086d40ac0737b48300f46bbf8
John Harlow <info@jharlow.uk>
2022-12-05 19:40:15 -0800

    Day 5: 2x ⭐

--- src/day05.js
+++ src/day05.js
@@ -1,23 +1,13 @@
 import fs from "fs";
 
-const testData = `    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
-
-move 1 from 2 to 1
-move 3 from 1 to 3
-move 2 from 2 to 1
-move 1 from 1 to 2`;
-
 const data = fs.readFileSync("./resources/day05.input.txt", "utf-8");
-const emptyNewLineRegex = /(?:\h*\n){2,}/;
-// crates[x: 0 = left][y: 0 = bottom]
-const crates = data
-  .split(emptyNewLineRegex)[0]
+
+const parse = (data) => {
+  let [map, instructions] = data.split("\n\n");
+  map = map
   .split(/\n/)
   .map((crateLevel) => crateLevel.match(/.{1,4}/g))
-  .slice(0, -1)
+  .slice(0, map.length-1)
   .map((_, i, arr) =>
     // change each array from being a row to a column
     arr
@@ -26,33 +16,26 @@ const crates = data
       .map((item) => item.match(/[A-Z]/i)[0])
       .reverse()
   )
-  .concat([[], [], []]);
 
-// [instructions] 0 = move, 1 = from, 2 = to
-const instructions = data
-  .split(emptyNewLineRegex)[1]
-  .split(/\n/)
-  .map((line) =>
-    line
+  instructions = instructions.split(/\n/).map((line) => {
+    const info = line
       .split(" ")
       .filter((item) => Number(item))
-      .map((n) => Number(n))
-  );
-
-const completeInstruction = (crates, instruction) => {
-  const move = instruction[0];
-  const from = instruction[1] - 1;
-  const to = instruction[2] - 1;
-  const movingCrates = crates[from].splice(-move).reverse()
-  crates[to].push(...movingCrates)
+      .map((n) => Number(n));
+    return { move: info[0], from: info[1] - 1, to: info[2] - 1 };
+  });
+  console.log(map)
+  return { crates: map, instructions };
 };
 
-const day05 = () => {
-  instructions.forEach((instruction) =>
-    completeInstruction(crates, instruction)
-  );
-  return crates.map((stack) => stack[stack.length - 1]).join("");
+const day05 = (input, singles = true) => {
+  const { crates, instructions } = parse(input);
+  for (const { move, from, to } of instructions) {
+    const removed = crates[from].splice(crates[from].length - move);
+    if (singles) removed.reverse();
+    crates[to].push(...removed);
+  }
+  return crates.map((x) => x.pop()).join("");
 };
 
-console.log(day05());
-console.table(crates);
+console.log(day05(data, true));
